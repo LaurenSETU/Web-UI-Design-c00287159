d9e8d51db4e680f52c2c8d9698204de1e415ce89
Lauren Brophy <c00287159@setu.ie>
2024-03-14 20:09:55 +0000

    fixed window pop up

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 window.onload= function(){
     alert('Hello Farmer! Ready to learn some things about the wonderful world of stardew?');
     };
+
+    function openUpdate(){
+        window.open('https://www.youtube.com/watch?v=LjLrUsMKc8w', 'newUpdate', 'width=800,heigth=600');
+    }
