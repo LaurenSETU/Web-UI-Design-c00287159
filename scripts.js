2105d41417a0082f438a3c0f58f6adeb2d250690
Lauren Brophy <c00287159@setu.ie>
2024-03-14 22:46:23 +0000

    lab 4 finished

--- scripts.js
+++ scripts.js
@@ -3,9 +3,16 @@ window.onload= function(){
     };
 
     function openUpdate(){
-        window.open('https://www.youtube.com/watch?v=LjLrUsMKc8w', 'newUpdate', 'width=800,heigth=600');
-    }
+        window.open('https://www.youtube.com/watch?v=LjLrUsMKc8w', 'newUpdate', 'width=800,height=600');
+    };
 
     function redirectToWebsite(){
         window.location.href = "https://www.stardewvalley.net/author/concernedape/";
-    }
+    };
+
+    document.querySelector('.button').addEventListener('mouseover',function(){
+        this.style.backgroundColor='lightblue';
+    });
+    document.querySelector('.button').addEventListener('mouseout',function(){
+        this.style.backgroundColor='';
+    });
